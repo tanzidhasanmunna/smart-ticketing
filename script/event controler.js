7069762961d86815da19c72d3f18da2a6e751d49
tanzidhasanmunna <tanzidhasanmunna@gmail.com>
2024-09-17 14:58:18 +0600

    d2

--- script/event controler.js
+++ script/event controler.js
@@ -2,12 +2,11 @@ document.getElementById("buy-tickets").addEventListener("click", function () {
   const buyTicketsElement = document.getElementById("order-section");
   buyTicketsElement.scrollIntoView({ behavior: "smooth", block: "start" });
 });
-let seat = [];
+
+let seats = [];
 
 function addSeat(value) {
-
-
-  
   setBackgroundColor(value);
-  console.log(seat)
+  console.log(seats);
 }
+
--- script/utility.js
+++ script/utility.js
@@ -4,12 +4,33 @@ function setBackgroundColor(element){
     if (element.style.color === 'white'){
         element.style.removeProperty('background-color');
         element.style.removeProperty('color');
-        const ind = seat.indexOf(element.id);
-        seat.splice(ind, 1);
+        const ind = seats.indexOf(element.id);
+        seats.splice(ind, 1);
+        const newId = element.id.toLowerCase();
+        popElement(newId);
+         
     }
     else{
         element.style.backgroundColor = '#1DD100';
         element.style.color = 'white';
-        seat.push(element.id);
+        seats.push(element.id);
+        setElement(element.id);
+          
     }
 }
+
+
+ function setElement(seat){
+    const seatContainer = document.getElementById('seat-selected');
+    const newId = seat.toLowerCase();
+        seatContainer.innerHTML += `<div id="${newId}" class="flex justify-between py-4">
+                                    <p>${seat}</p>
+                                    <p>Economy</p>
+                                    <p>550</p>
+                                </div>`
+    
+ }
+ function popElement( element ){
+    document.getElementById(element).classList.add('hidden')
+}
+ 
